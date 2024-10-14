6ce52c03579b3f9afef87064270992e048876430
udqy <udaygitlog@gmail.com>
2024-10-15 02:53:18 +0530

    Update Model and Urls to temporarily remove Order model

--- upload/models.py
+++ upload/models.py
@@ -1,23 +1,33 @@
 from django.db import models
 
 class Product(models.Model):
-    product_id = models.AutoField(primary_key=True)  # Auto increment
-    product_name = models.CharField(max_length=255, unique=True)
+    product_id = models.AutoField(primary_key=True, default=0)
+    product_name = models.CharField(max_length=255, unique=True, default='')
 
     def __str__(self):
         return self.product_name
-    
+
     class Meta:
         db_table = "products"
 
-class Order(models.Model):
-    order_id = models.AutoField(primary_key=True)
-    product_id = models.IntegerField()  # Assuming product_id is an integer
-    product_name = models.CharField(max_length=255)
+class Ingredient(models.Model):
+    ingredient_id = models.AutoField(primary_key=True, default=0)
+    ingredient_name = models.CharField(max_length=255, unique=True, default='')
+
+    def __str__(self):
+        return self.ingredient_name
+
+    class Meta:
+        db_table = "ingredients"
+
+class Composition(models.Model):
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.FloatField()
 
     def __str__(self):
-        return f'Order {self.order_id} - {self.product_name} ({self.quantity})'
+        return f"{self.product.product_name} - {self.ingredient.ingredient_name} ({self.quantity})"
 
     class Meta:
-        db_table = "orders"
+        db_table = "composition"
+        unique_together = ('ingredient', 'product')
