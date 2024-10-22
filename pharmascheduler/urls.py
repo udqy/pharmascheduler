121db858a464d2023462c3c2bfae1dba64d2e240
udqy <udaygitlog@gmail.com>
2024-10-23 03:39:12 +0530

    Minor fixes - final commit - amen

--- pharmascheduler/urls.py
+++ pharmascheduler/urls.py
@@ -1,6 +1,8 @@
+from django.conf import settings
 from django.contrib import admin
 from django.views.generic import RedirectView
 from django.urls import include, path
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,3 +17,6 @@ urlpatterns = [
 
     path('inventory/', include('inventory.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
