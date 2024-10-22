87c1367f9c99c7f3bc6c0ecd2bdec2e292aa4b3a
udqy <udaygitlog@gmail.com>
2024-10-23 03:55:03 +0530

    Add default routing url after login

--- schedule/urls.py
+++ schedule/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.upload_schedule, name='upload_schedule'),
     path('upload/', views.upload_schedule, name='upload_schedule'),
     path('view/<int:schedule_id>/', views.view_schedule, name='view_schedule'),
 ]
